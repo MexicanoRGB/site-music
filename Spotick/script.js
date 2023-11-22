baaeaf9db788fd87ca5bdf7ad97e1dee85dca03c
MexicanoRGB <124815875+MexicanoRGB@users.noreply.github.com>
2023-11-22 15:49:51 -0300

    Add files via upload

---
+++ Spotick/script.js
@@ -0,0 +1,11 @@
+const audio = document.getElementById('audio');
+
+const songs = document.querySelectorAll('.music');
+
+songs.forEach(song =>{
+    song.addEventListener('click',() =>{
+        const src = song.getAttribute('data-src');
+        audio.src = src;
+        audio.play();
+    })
+});
